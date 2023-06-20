c0927046d2e95f37ee6f90df69c8490679f19205
Natzu83 <natzuac@yahoo.com>
2023-06-20 15:43:49 +0000

    Add space

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,18 +11,18 @@
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	if(n<0)
+	if(n > 0)
 	{
 		printf("%d is a positive number\n", n);
 	}
-	else if(n>0)
+	else if(n < 0)
 	{
-		printf("%d is a negative number\n", n);
+        printf("%d is a negative number\n", n);
 	}
-	else(n==0)
+	else if (n == 0) 
 	{
-		printf("%d is zero\n", n);
+        printf("%d is zero\n", n);
 	}
-	
+
 	return (0);
 }
