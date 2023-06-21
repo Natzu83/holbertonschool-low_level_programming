a8d44d874efcf5182a6f29d3b25bdbb837134cec
Natzu83 <natzuac@yahoo.com>
2023-06-21 19:16:23 +0000

    Help

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,9 +1,9 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
- * main- Program will print the digital number stored in the n var.
+ * main- Entry point
  * Return: always 0
  */
 
@@ -25,6 +25,6 @@ int main(void)
 	else if (lastd < 6 && lastd != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
-		{
-			return (0);
-		}
+	}
+	return (0);
+}
