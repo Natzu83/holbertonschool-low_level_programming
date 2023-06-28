67bc648556b61a627f1f773868c5350ef225809b
Natzu83 <natzuac@yahoo.com>
2023-06-28 13:24:18 +0000

    98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Make it 98
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+	int *p;
+
+	n = 98;
+	p = &n;
+	printf("Value of 'n' : %d\n", n);
+	printf("Adress of 'n': %p\n", &n);
+	printf("Value of 'p': %p\n", p);
+	*p = 402;
+	printf("Value of 'n': %d\n", n);
+	return (0);
+}
