078b80d3305277d80439659f2d24c8511136cd3d
Natzu83 <natzuac@yahoo.com>
2023-06-28 15:27:20 +0000

    Change names

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -4,18 +4,18 @@
 
 /**
  * main - int _strlen(char *s)
- * int length is variable name
- * lenght++ means it increments
- * s++ same as lenght var
+ * @int n is variable name
+ * @lenght++ means it increments
+ * @s++ same as lenght var
  * Return: Always 0.
  */
 
 int _strlen(char *s)
 {
-	int length = 0;
+	int n = 0;
 	while (*s != '\0')
 	{
-		lenght++;
+		n++;
 		s++;
 	}
 
