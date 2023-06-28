5be86fb459c33ee4c9346324d75c8705fa458907
Natzu83 <natzuac@yahoo.com>
2023-06-28 17:29:49 +0000

    Time to put

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - void _puts(char *str)
+ * @*str Is the value assigned via int
+ * Return: Always 0.
+ */
+
+void _puts(char *str);
+{
+	int *str;
+
+	*str = "I do not fear computers.";
+	_puts(str);
+	return (0)
+}
