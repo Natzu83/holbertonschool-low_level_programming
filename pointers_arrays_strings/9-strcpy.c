904364d3df8815a02327fa95b291bfa9020568f2
Natzu83 <natzuac@yahoo.com>
2023-06-30 15:19:54 +0000

    Is all in the copy string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Write a function
+ * @char: Is a data type for characters
+ * @dest: Return the value to the pointer
+ * @src: Include the statment of the null byte
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
