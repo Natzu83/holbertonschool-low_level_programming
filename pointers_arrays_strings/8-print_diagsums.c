78031a645eaf9f8bdca2574e0a8ab55f06053553
Natzu83 <natzuac@yahoo.com>
2023-07-03 15:25:37 +0000

    Betty

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -15,9 +15,9 @@ void print_diagsums(int *a, int size)
 	int sum2 = 0;
 
 	for (i = 0; i < size * size; i = i + size + 1, j = j + size - 1)
-        {
-                sum1 = sum1 + a[i];
-                sum2 = sum2 + a[j];
-        }
-        printf("%i, %i\n", sum1, sum2);
+	{
+		sum1 = sum1 + a[i];
+		sum2 = sum2 + a[j];
+	}
+	printf("%i, %i\n", sum1, sum2);
 }
