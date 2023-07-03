19efef77f8e6494924fc2ed98a48e70f47c55003
Natzu83 <natzuac@yahoo.com>
2023-07-03 15:30:41 +0000

    Chess

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -4,17 +4,19 @@
 /**
  * print_chessboard - Print the chess
  * @a: Variable to use the rows
- * 8: Variable use to make the columns
  * Return: always 0
  */
-void printChessboard(char (*a)[8])
+void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int b, c;
 
-	for (i = 0; i < 8; ++j)
+	for (b = 0; b < 8; b++)
 	{
-		for (j = 0; j < 8, ++i)
-			_putchar(a[i][j]);
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+
 		_putchar('\n');
 	}
 }
