b7150953f41d9f1cb6082516df51d8a9990f8038
Natzu83 <natzuac@yahoo.com>
2023-07-03 13:37:53 +0000

    Byte string

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strspn Write - The Prototype for the substring lenght
+ * @char *s: Char S variable
+ * @char *accept: Gives the bytes
+ * Return: void
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+char *accept  = strspn(char *s);
+
+printf("Length of bytes: %zu\n", accept);
+
+return 0;
+}
+
