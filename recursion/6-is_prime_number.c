9a275b9eb80514e9109a1b06f37216c8c2b27bd1
Natzu83 <natzuac@yahoo.com>
2023-07-06 17:39:40 +0000

    Edits

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,23 +8,23 @@
  */
 int is_prime_number(int n)
 {
-	return (extra_prime(n, 2, n / 2));
+	return (helper_prime(n, 2, n / 2));
 }
 
 /**
- * extra_prime - extra
+ *  helper_prime - blank
  * @n: checkl
  * @i: for the increment
  * @limit: stop
  * Return: 0
  */
 
-int extra_prime(int n, int i, int limit)
+int helper_prime(int n, int i, int limit)
 {
 	if ((n % i == 0 && i <= limit) || n < 0 || n == 1)
 		return (0);
 	else if (n % i != 0 && i <= limit)
-		return (extra_prime(n, i + 1, limit));
+		return (helper_prime(n, i + 1, limit));
 	else
 		return (1);
 }
