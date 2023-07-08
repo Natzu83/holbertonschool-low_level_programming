d8cbca9d30c1ff8f119d3f5f610635872dd41678
Natzu83 <natzuac@yahoo.com>
2023-07-08 03:33:42 +0000

    Work plz

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,34 +9,43 @@
  *
  * Return: 0, 1 if non-digits are passed
  */
+#include <stdio.h>
+#include <stdlib.h>
+
 int is_digit(char c)
 {
-	return (c >= '0' && c <= '9');
+    return (c >= '0' && c <= '9');
 }
+
 int main(int argc, char *argv[])
 {
-	int i, number, sum = 0;
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
-	
-	for (i = 1; i < argc; i++)
-	{
-		int j = 0;
-		while (argv[i][j] != '\0')
-	}
-	if (!is_digit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			j++;
-		}
-		number = atoi(argv[i]);
-		sum += number;
-	}
-	printf("%d\n", sum);
-	return 0;
+    int i, number, sum = 0;
+
+    if (argc == 1)
+    {
+        printf("0\n");
+        return 0;
+    }
+
+    for (i = 1; i < argc; i++)
+    {
+        int j = 0;
+
+        while (argv[i][j] != '\0')
+        {
+            if (!is_digit(argv[i][j]))
+            {
+                printf("Error\n");
+                return 1;
+            }
+            j++;
+        }
+
+        number = atoi(argv[i]);
+        sum += number;
+    }
+
+    printf("%d\n", sum);
+    return 0;
 }
+
