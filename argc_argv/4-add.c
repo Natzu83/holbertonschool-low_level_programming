457e79315dfdc40c02b6550270556aac099178cc
Natzu83 <natzuac@yahoo.com>
2023-07-08 03:38:21 +0000

    Plz work

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,51 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
-
 /**
- * main - Adds positive numbers
- * @argc: Number of command line arguments
- * @argv: Array containing command line arguments
- *
- * Return: 0, 1 if non-digits are passed
+ * main - program that prints the name followed by a new line
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
+ * Return: 0
  */
-#include <stdio.h>
-#include <stdlib.h>
-
-int is_digit(char c)
-{
-    return (c >= '0' && c <= '9');
-}
-
 int main(int argc, char *argv[])
 {
-    int i, number, sum = 0;
+	int sum, val, i;
 
-    if (argc == 1)
-    {
-        printf("0\n");
-        return 0;
-    }
+	sum = 0;
+	if (argc < 1)
+		printf("%d\n", 0);
+	while (argc-- && argc > 0)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+		{
 
-    for (i = 1; i < argc; i++)
-    {
-        int j = 0;
-
-        while (argv[i][j] != '\0')
-        {
-            if (!is_digit(argv[i][j]))
-            {
-                printf("Error\n");
-                return 1;
-            }
-            j++;
-        }
-
-        number = atoi(argv[i]);
-        sum += number;
-    }
-
-    printf("%d\n", sum);
-    return 0;
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		val = atoi(argv[argc]);
+		sum += val;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
-
