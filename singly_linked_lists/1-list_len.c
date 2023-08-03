3ae518887de7637cd25071eea3de52341016561a
Natzu83 <natzuac@yahoo.com>
2023-08-03 14:37:26 +0000

    Lenght of the list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,19 +8,14 @@
  * @h:Linked list
  * Return: Number of elements
  */
-typedef struct list_t {
-    int data;
-    struct list_t *next;
-} list_t;
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
 
-size_t list_len(const list_t *h) {
-    size_t count = 0;
-
-    while (h != NULL) {
-        count++;
-        h = h->next;
-    }
-
-    return count;
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
-
