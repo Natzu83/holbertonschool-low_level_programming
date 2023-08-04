a33ef0588cccd129d36c198150d53291a43ed204
Natzu83 <natzuac@yahoo.com>
2023-08-04 23:00:45 +0000

    List

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -2,17 +2,17 @@
 #define _S_LINK_LISTS_
 
 /**
- * struct listint_s - singly linked list
+ * struct dlistint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
-typedef struct listint_s
+typedef struct dlistint_s
 {
 	int n;
-	struct listint_s *next;
-} listint_t;
+	struct dlistint_s *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t listint_len(const listint_t *h);
