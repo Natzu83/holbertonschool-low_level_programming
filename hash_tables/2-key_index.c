e95693b6ea6894d1f34a9d823e5665a2750e1145
Natzu83 <natzuac@yahoo.com>
2023-08-15 16:28:07 +0000

    Index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
  * key_index - Function that gives the key's index
- * @key: Is the key in the hash table
- * @size: is the Hash Table Size
+ * @key: The key in the hash table
+ * @size: Hash Table Size
  * Return: Key index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
