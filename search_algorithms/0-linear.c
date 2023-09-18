5e8db88d8d96acfe1807209936cd10b8f202edb3
Natzu83 <natzuac@yahoo.com>
2023-09-18 12:56:11 +0000

    Linear

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	for (; i < size; ++i)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			break;
+	}
+
+	if (i == size)
+		return (-1);
+
+	return (i);
+}
