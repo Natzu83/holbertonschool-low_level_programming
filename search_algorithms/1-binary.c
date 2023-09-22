ce602d1f0ba868b2051aa191a1b3729aaaa5ac64
Natzu83 <natzuac@yahoo.com>
2023-09-22 12:09:36 +0000

    Binary

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+  * binary_search - Searches for a value in a sorted array of
+  * integers using the Binary search algorithm
+  *
+  * @array: A pointer to the first element of the array to search in
+  * @size: The number of elements in array
+  * @value: The value to search for
+  *
+  * Return: The first index where value is located
+  */
+
